085badda1885724a492f01b67515d6b00068a629
Samuel A. Yallop <syallop@gmail.com>
2016-04-14 18:02:26 +0100

    Exports library

---
+++ GameEngine.hs
@@ -0,0 +1,28 @@
+module GameEngine
+  (module G
+  )
+  where
+
+import qualified GameEngine.Agent                as G
+import qualified GameEngine.Background           as G
+import qualified GameEngine.Camera               as G
+import qualified GameEngine.Collect              as G
+import qualified GameEngine.ConfigReader         as G
+import qualified GameEngine.Counter              as G
+import qualified GameEngine.Force                as G
+import qualified GameEngine.HitBox               as G
+import qualified GameEngine.Live                 as G
+import qualified GameEngine.Position             as G
+import qualified GameEngine.Size                 as G
+import qualified GameEngine.Stage                as G
+import qualified GameEngine.StageConfigReader    as G
+import qualified GameEngine.Thing                as G
+import qualified GameEngine.ThingConfigReader    as G
+import qualified GameEngine.Tile                 as G
+import qualified GameEngine.TileConfigReader     as G
+import qualified GameEngine.TileGrid             as G
+import qualified GameEngine.TileGridConfigReader as G
+import qualified GameEngine.TileSet              as G
+import qualified GameEngine.TileSetConfigReader  as G
+import qualified GameEngine.Velocity             as G
+
